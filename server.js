120614efcbcc2dddb19eff435538e2f79e651ee1
x <>
2026-06-30 19:21:20 +0200

    test

--- server.js
+++ server.js
@@ -3,39 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCZYCGJNw1U58FG3bjRo87RLwJcerAGZi0",
-  authDomain: "saw-project-d2fad.firebaseapp.com",
-  projectId: "saw-project-d2fad",
-  storageBucket: "saw-project-d2fad.firebasestorage.app",
-  messagingSenderId: "406220704198",
-  appId: "1:406220704198:web:0ab51a2c2cf51676f88ac7",
-  measurementId: "G-5NCW7HZ9DJ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-
-/*const serviceAccount = require("./firebase-key.json");
+const serviceAccount = require("./firebase-key.json");
 initializeApp({
   credential: cert(serviceAccount)
-});*/
+});
 
-//const db = getFirestore();
-const apps = express();
-apps.use(cors());
-apps.use(bodyParser.json());
+const db = getFirestore();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 // 1. VAPID Keys (Generate once and reuse)
 const vapidKeys = webpush.generateVAPIDKeys();
@@ -47,14 +26,14 @@ webpush.setVapidDetails(
 
 console.log("Public Key for index.html:", vapidKeys.publicKey);
 
-apps.get('/vapidPublicKey', (req, res) => {
+app.get('/vapidPublicKey', (req, res) => {
   res.send(vapidKeys.publicKey);
 });
 
 // 2. In-memory storage for subscribers
 let subscribers = [];
 
-apps.post('/subscribe', (req, res) => {
+app.post('/subscribe', (req, res) => {
   const subscription = req.body;
   
   // Add the new subscription to our list
@@ -63,7 +42,6 @@ apps.post('/subscribe', (req, res) => {
   console.log("New user subscribed. Total subscribers:", subscribers.length);
   res.status(201).json({});
 });
-
 
 let serverPronto = false;
 
@@ -107,4 +85,4 @@ db.collection('projects').onSnapshot((snapshot) => {
   console.error("Errore nell'ascolto di Firestore:", error);
 });
 
-apps.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
